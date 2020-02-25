0d5bd934f06c4078d562149efa017df9957f852f
TeguhWibowo97 <teguhwibowo9797@gmail.com>
2020-02-25 22:31:18 +0700

    upload folder vendor

--- app/Http/Controllers/PenelitianController.php
+++ app/Http/Controllers/PenelitianController.php
@@ -14,7 +14,7 @@ class PenelitianController extends Controller
     public function index()
     {
         $penelitian = MPenelitian::all();
-        // dd($penelitian);
+         //dd($penelitian);
         return view('penelitian.coffee',['penelitian'=>$penelitian]);
     }
 
