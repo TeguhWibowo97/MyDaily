116528eafac4b5a6d40f49e19d0b4ecfe08663a6
TeguhWibowo97 <teguhwibowo9797@gmail.com>
2020-03-02 15:46:08 +0700

    memperbaiki file routes yg kepindah krna kepencet

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,17 @@ Route::get('/logout','AuthController@logout');
 Route::group(['middleware'=>'auth'], function(){
     
     Route::get('/dashboard','DashboardController@dashboard')->middleware('auth');
-    
+    // Penelitian Coffee
     Route::post('/penelitian/coffee/create','CoffeeController@store');
     Route::get('/penelitian/coffee','CoffeeController@index');
     Route::get('/penelitian/coffee/edit/{penelitian}','CoffeeController@edit');
     Route::post('/penelitian/coffee/edit/{penelitian}','CoffeeController@update');
     Route::get('/penelitian/coffee/hapus/{penelitian}','CoffeeController@destroy');
 
+    // Penelitian Alga
+    Route::post('/penelitian/alga/create','AlgaController@store');
+    Route::get('/penelitian/alga','AlgaController@index');
+    Route::get('/penelitian/alga/edit/{penelitian}','AlgaController@edit');
+    Route::post('/penelitian/alga/edit/{penelitian}','AlgaController@update');
+    Route::get('/penelitian/alga/hapus/{penelitian}','AlgaController@destroy');
 });
